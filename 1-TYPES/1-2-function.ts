10efc51025d721b2764e1b0e0e11802ff83de57c
kkomyoung <kya754@gmail.com>
2023-06-21 23:59:23 +0900

    20230621: 함수 타입 이용

--- 1-TYPES/1-2-function.ts
+++ 1-TYPES/1-2-function.ts
@@ -1,32 +1,43 @@
 {
     // JavaScript 💩
-    function jsAdd(num1, num2) {
-        return num1 + num2;
+    // function jsAdd(num1, num2) {
+    //     return num1 + num2;
+    // }
+
+    // // TypeScript
+    // function add(num1: number, num2: number): number {
+    //     return num1 + num2
+    // }
+
+    // // JavaScript 💩
+    // function jsFetchNum(id) {
+    //     // code ...
+    //     // code ...
+    //     // code ...
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     });
+    // }
+
+    // // TypeScript
+    // // tip) id는 보통 string으로 많이 씀
+    // function fetchNum(id: string): Promise<number> {
+    //     // code ...
+    //     // code ...
+    //     // code ...
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     });
+    // }
+
+    // JavaScript => TypeScript
+    // Optional parameter
+    function printName(firstName: string, lastName?: string) {
+        console.log(firstName);
+        console.log(lastName);
     }
 
-    // TypeScript
-    function add(num1: number, num2: number): number {
-        return num1 + num2
-    }
-
-    // JavaScript 💩
-    function jsFetchNum(id) {
-        // code ...
-        // code ...
-        // code ...
-        return new Promise((resolve, reject) => {
-            resolve(100);
-        });
-    }
-
-    // TypeScript
-    // tip) id는 보통 string으로 많이 씀
-    function fetchNum(id: string): Promise<number> {
-        // code ...
-        // code ...
-        // code ...
-        return new Promise((resolve, reject) => {
-            resolve(100);
-        });
-    }
+    printName('Steve', 'Jobs');
+    printName('Ellie');
+    printName('Anna', undefined);
 }
