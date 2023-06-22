1baee3295834e7ee0560c097de04d2a2f5264e14
kkomyoung <kya754@gmail.com>
2023-06-22 14:49:10 +0900

    20230622: 함수에서 타입 이용하기 (JS 💩 → TS ✨)

--- 1-TYPES/1-2-function.ts
+++ 1-TYPES/1-2-function.ts
@@ -32,12 +32,11 @@
 
     // JavaScript => TypeScript
     // Optional parameter
-    // lastName: 전달 받을 수도 있고 받지 않을 수도 있다.
     function printName(firstName: string, lastName?: string) { 
         console.log(firstName);
         console.log(lastName);
     }
-
+    // lastName: 전달 받을 수도 있고 받지 않을 수도 있다.
     printName('Steve', 'Jobs');
     printName('Ellie');
     printName('Anna', undefined);
